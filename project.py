1f4c3f73ceb7f58441f2111779be0c4757fb40ad
CarlosGenis <carlosgenis06@gmail.com>
2021-12-09 16:22:45 -0500

    added required sections

--- project.py
+++ project.py
@@ -1,9 +1,11 @@
 #Carlos Genis
-#Resources:  Used python.org as a reminder of Python 3 print statements.
-#carlos.genis34@myhunter.cuny.edu 
-import pandas as pd 
+#Resources: https://data.cityofnewyork.us/Transportation/Open-Restaurant-Applications/pitm-atqc/data
+#URL: https://carlosgenis.github.io/FinalProject/
+#Title: An in-depth look into NYC open restaurant applications
+#carlos.genis34@myhunter.cuny.edu
+import pandas as pd
 
-#read file 
+#read file
 df = pd.read_csv('Open_Restaurant_Applications.csv')
 #print (df)
 #got total from amount of rows
@@ -12,19 +14,19 @@ print("Total applications submitted:", total_applications)
 #get all sidewalk seatings
 sidewalk_seatings = df['Approved for Sidewalk Seating'].value_counts()
 print("Sidewalk Seating Application Results: ")
-#get approval percentage for sidewalk seatings 
+#get approval percentage for sidewalk seatings
 print(sidewalk_seatings)
-sidewalk_acceptance_rate = (10963 / total_applications) * 100 
+sidewalk_acceptance_rate = (10963 / total_applications) * 100
 sidewalk_acceptance_rate = round(sidewalk_acceptance_rate, 2)
 print("Percentage of sidewalk seating applications approved:", sidewalk_acceptance_rate, "%")
 
 
-#get all roadwalk seatings 
-roadwalk_seatings = df['Approved for Roadway Seating'].value_counts()
-print("Roadwalk Seating Application Results: ", roadwalk_seatings)
-roadwalk_acceptance_rate = (8335/ total_applications) * 100 
-roadwalk_acceptance_rate = round(roadwalk_acceptance_rate, 2)
-print("Percentage of roadwalk seating applications approved: ", roadwalk_acceptance_rate, "%")
+#get all roadway seatings
+roadway_seatings = df['Approved for Roadway Seating'].value_counts()
+print("Roadway Seating Application Results: ", roadway_seatings)
+roadway_acceptance_rate = (8335/ total_applications) * 100
+roadway_acceptance_rate = round(roadway_acceptance_rate, 2)
+print("Percentage of roadway seating applications approved: ", roadway_acceptance_rate, "%")
 
 #get queens data
 queens = df.loc[df['Borough'] == 'Queens']
@@ -37,17 +39,17 @@ sidewalk_seatings_queens = queens['Approved for Sidewalk Seating'].value_counts(
 print("Queens Sidewalk Seating Application Results: ")
 #get approval percentage for sidewalk seatings for queens
 print(sidewalk_seatings_queens)
-sidewalk_acceptance_rate_queens = (2275 / total_applications_queens) * 100 
+sidewalk_acceptance_rate_queens = (2275 / total_applications_queens) * 100
 sidewalk_acceptance_rate_queens = round(sidewalk_acceptance_rate_queens, 2)
 print("Percentage of sidewalk seating applications approved for Queens :", sidewalk_acceptance_rate_queens, "%")
-#get queens roadwalk seatings 
-roadwalk_seatings_queens = queens['Approved for Roadway Seating'].value_counts()
-print("Roadwalk Seating Application Results for Queens : ", roadwalk_seatings_queens)
-roadwalk_acceptance_rate_queens = (1601/ total_applications_queens) * 100 
-roadwalk_acceptance_rate_queens = round(roadwalk_acceptance_rate_queens, 2)
-print("Percentage of roadwalk seating applications approved for Queens : ", roadwalk_acceptance_rate_queens, "%")
+#get queens roadway seatings
+roadway_seatings_queens = queens['Approved for Roadway Seating'].value_counts()
+print("Roadway Seating Application Results for Queens : ", roadway_seatings_queens)
+roadway_acceptance_rate_queens = (1601/ total_applications_queens) * 100
+roadway_acceptance_rate_queens = round(roadway_acceptance_rate_queens, 2)
+print("Percentage of roadway seating applications approved for Queens : ", roadway_acceptance_rate_queens, "%")
 
-# get manhattan data 
+# get manhattan data
 manhattan = df.loc[df['Borough'] == 'Manhattan']
 #print(manhattan)
 total_applications_manhattan = manhattan.shape[0]
@@ -56,36 +58,36 @@ print("Total applications submitted for Manhattan: ", total_applications_manhatt
 sidewalk_seatings_manhattan = manhattan['Approved for Sidewalk Seating'].value_counts()
 print("Manhattan Sidewalk Seating Application Results: ", sidewalk_seatings_manhattan)
 #get approval percentage for sidewalk seatings for manhattan
-sidewalk_acceptance_rate_manhattan = (5187 / total_applications_manhattan) * 100 
+sidewalk_acceptance_rate_manhattan = (5187 / total_applications_manhattan) * 100
 sidewalk_acceptance_rate_manhattan = round(sidewalk_acceptance_rate_manhattan, 2)
 print("Percentage of sidewalk seating applications approved for Manhattan :", sidewalk_acceptance_rate_manhattan, "%")
-#get manhattan roadwalk seatings 
-roadwalk_seatings_manhattan = manhattan['Approved for Roadway Seating'].value_counts()
-print("Roadwalk Seating Application Results for Manhattan : ", roadwalk_seatings_manhattan)
-roadwalk_acceptance_rate_manhattan = (4176/ total_applications_manhattan) * 100 
-roadwalk_acceptance_rate_manhattan = round(roadwalk_acceptance_rate_manhattan, 2)
-print("Percentage of roadwalk seating applications approved for Manhattan : ", roadwalk_acceptance_rate_manhattan, "%")
+#get manhattan roadway seatings
+roadway_seatings_manhattan = manhattan['Approved for Roadway Seating'].value_counts()
+print("Roadway Seating Application Results for Manhattan : ", roadway_seatings_manhattan)
+roadway_acceptance_rate_manhattan = (4176/ total_applications_manhattan) * 100
+roadway_acceptance_rate_manhattan = round(roadway_acceptance_rate_manhattan, 2)
+print("Percentage of roadway seating applications approved for Manhattan : ", roadway_acceptance_rate_manhattan, "%")
 
-#get brooklyn data 
+#get brooklyn data
 brooklyn = df.loc[df['Borough'] == 'Brooklyn']
 #print(brooklyn)
 total_applications_brooklyn = brooklyn.shape[0]
 print("Total applications submitted for Brooklyn: ", total_applications_brooklyn)
-#get all sidewalk seatings for brooklyn 
+#get all sidewalk seatings for brooklyn
 sidewalk_seatings_brooklyn = brooklyn['Approved for Sidewalk Seating'].value_counts()
 print("Brooklyn Sidewalk Seating Application Results: ", sidewalk_seatings_brooklyn)
 #get approval percentage for sidewalk seatings for brooklyn
-sidewalk_acceptance_rate_brooklyn = (2675 / total_applications_brooklyn) * 100 
+sidewalk_acceptance_rate_brooklyn = (2675 / total_applications_brooklyn) * 100
 sidewalk_acceptance_rate_brooklyn = round(sidewalk_acceptance_rate_brooklyn, 2)
 print("Percentage of sidewalk seating applications approved for Brooklyn :", sidewalk_acceptance_rate_brooklyn, "%")
-#get brooklyn roadwalk seatings 
-roadwalk_seatings_brooklyn = brooklyn['Approved for Roadway Seating'].value_counts()
-print("Roadwalk Seating Application Results for Brooklyn : ", roadwalk_seatings_brooklyn)
-roadwalk_acceptance_rate_brooklyn = (2060/ total_applications_brooklyn) * 100 
-roadwalk_acceptance_rate_brooklyn = round(roadwalk_acceptance_rate_brooklyn, 2)
-print("Percentage of roadwalk seating applications approved for Brooklyn : ", roadwalk_acceptance_rate_brooklyn, "%")
+#get brooklyn roadway seatings
+roadway_seatings_brooklyn = brooklyn['Approved for Roadway Seating'].value_counts()
+print("Roadway Seating Application Results for Brooklyn : ", roadway_seatings_brooklyn)
+roadway_acceptance_rate_brooklyn = (2060/ total_applications_brooklyn) * 100
+roadway_acceptance_rate_brooklyn = round(roadway_acceptance_rate_brooklyn, 2)
+print("Percentage of roadway seating applications approved for Brooklyn : ", roadway_acceptance_rate_brooklyn, "%")
 
-#get bronx data 
+#get bronx data
 bronx = df.loc[df['Borough'] == 'Bronx']
 #print(bronx)
 total_applications_bronx = bronx.shape[0]
@@ -94,15 +96,15 @@ print("Total applications submitted for the Bronx: ", total_applications_bronx)
 sidewalk_seatings_bronx = bronx['Approved for Sidewalk Seating'].value_counts()
 print("Bronx Sidewalk Seating Application Results: ", sidewalk_seatings_bronx)
 #get approval percentage for sidewalk seatings for bronx
-sidewalk_acceptance_rate_bronx = (648 / total_applications_bronx) * 100 
+sidewalk_acceptance_rate_bronx = (648 / total_applications_bronx) * 100
 sidewalk_acceptance_rate_bronx = round(sidewalk_acceptance_rate_bronx, 2)
 print("Percentage of sidewalk seating applications approved for the Bronx :", sidewalk_acceptance_rate_bronx, "%")
-#get bronx roadwalk seatings 
-roadwalk_seatings_bronx = bronx['Approved for Roadway Seating'].value_counts()
-print("Roadwalk Seating Application Results for the Bronx : ", roadwalk_seatings_bronx)
-roadwalk_acceptance_rate_bronx = (402/ total_applications_bronx) * 100 
-roadwalk_acceptance_rate_bronx = round(roadwalk_acceptance_rate_bronx, 2)
-print("Percentage of roadwalk seating applications approved for the Bronx : ", roadwalk_acceptance_rate_bronx, "%")
+#get bronx roadway seatings
+roadway_seatings_bronx = bronx['Approved for Roadway Seating'].value_counts()
+print("Roadway Seating Application Results for the Bronx : ", roadway_seatings_bronx)
+roadway_acceptance_rate_bronx = (402/ total_applications_bronx) * 100
+roadway_acceptance_rate_bronx = round(roadway_acceptance_rate_bronx, 2)
+print("Percentage of roadway seating applications approved for the Bronx : ", roadway_acceptance_rate_bronx, "%")
 
 #get staten island data
 staten_island = df.loc[df['Borough'] == 'Staten Island']
@@ -113,14 +115,12 @@ print("Total applications submitted for Staten Island : ", total_applications_st
 sidewalk_seatings_staten_island = staten_island['Approved for Sidewalk Seating'].value_counts()
 print("Staten Island Sidewalk Seating Application Results: ", sidewalk_seatings_staten_island)
 #get approval percentage for sidewalk seatings for staten island
-sidewalk_acceptance_rate_staten_island = (178 / total_applications_staten_island) * 100 
+sidewalk_acceptance_rate_staten_island = (178 / total_applications_staten_island) * 100
 sidewalk_acceptance_rate_staten_island = round(sidewalk_acceptance_rate_staten_island, 2)
 print("Percentage of sidewalk seating applications approved for Staten Island :", sidewalk_acceptance_rate_staten_island, "%")
-#get staten island roadwalk seatings 
-roadwalk_seatings_staten_island = staten_island['Approved for Roadway Seating'].value_counts()
-print("Roadwalk Seating Application Results for Staten Island : ", roadwalk_seatings_staten_island)
-roadwalk_acceptance_rate_staten_island = (96/ total_applications_staten_island) * 100 
-roadwalk_acceptance_rate_staten_island = round(roadwalk_acceptance_rate_staten_island, 2)
-print("Percentage of roadwalk seating applications approved for Staten Island : ", roadwalk_acceptance_rate_staten_island, "%")
-
-
+#get staten island roadway seatings
+roadway_seatings_staten_island = staten_island['Approved for Roadway Seating'].value_counts()
+print("Roadway Seating Application Results for Staten Island : ", roadway_seatings_staten_island)
+roadway_acceptance_rate_staten_island = (96/ total_applications_staten_island) * 100
+roadway_acceptance_rate_staten_island = round(roadway_acceptance_rate_staten_island, 2)
+print("Percentage of roadway seating applications approved for Staten Island : ", roadway_acceptance_rate_staten_island, "%")
